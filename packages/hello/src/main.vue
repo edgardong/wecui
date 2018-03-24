1a4d783e6ec205bc8df89a806fa8dce5f1c8d09e
edgardong <2535615874@qq.com>
2018-03-24 20:58:43 +0800

    modify test files

--- packages/hello/src/main.vue
+++ packages/hello/src/main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wec-hello">
+  <div class="wec-hello" @click="handleClick">
     <p class="wec-hello__message">{{ message }}</p> 
   </div>
 </template>
@@ -9,6 +9,15 @@ export default {
   name: 'wecHello',
   props: {
     message: String
+  },
+  methods: {
+    handleClick () {
+      return new Promise((resolve) => {
+        resolve()
+      }).then(() => {
+        this.$emit('click', 'this is click emit')
+      })
+    }
   }
 }
 </script>
