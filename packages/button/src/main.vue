6604941ae11b96156c2b957e6f220b538e6654e9
edgardong <2535615874@qq.com>
2018-05-10 21:58:20 +0800

    add header

--- packages/button/src/main.vue
+++ packages/button/src/main.vue
@@ -1,10 +1,33 @@
 <template>
   <div class="wec-button">
-    hello button~~~
+    <button class="button" :class="[type?'button-' + type:'', size? 'button-'+ size:'']"></button>
   </div>
 </template>
 <script>
 export default {
-  name: 'wec-button'
-}
+  name: "wec-button",
+  props: {
+    // 尺寸
+    size: {
+      type: String,
+      default: "middle",
+      validator: function(value) {
+        // 值必须是这些字符串中的一个
+        return ["small", "middle", "large"].indexOf(value) !== -1;
+      }
+    },
+    // 类型
+    type: {
+      type: String,
+      default: "primary",
+      validator: function(value) {
+        return ["primary", "warn", "success", "error"].indexOf(value) !== -1;
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {}
+};
 </script>
