947927049424f395abdeaa2a3edf2498448569a6
edgardong <2535615874@qq.com>
2018-06-05 22:54:16 +0800

    777

--- examples/pages/button.vue
+++ examples/pages/button.vue
@@ -1,14 +1,24 @@
 <template>
   <section class="demo-button">
-      <wec-button>默认按钮</wec-button>
-      <wec-button size="small">小按钮</wec-button>
-      <wec-button size="middle">常规按钮</wec-button>
-      <wec-button size="large">大按钮</wec-button>
+      <div class="button-item"><wec-button>默认按钮</wec-button></div>
+      <div class="button-item"><wec-button size="mini">小按钮</wec-button></div>
+      <div class="button-item"><wec-button size="middle">常规按钮</wec-button></div>
+      <div class="button-item"><wec-button size="large">大按钮</wec-button></div>
 
-      <wec-button type="primary">primary</wec-button>
-      <wec-button type="warn">warn</wec-button>
-      <wec-button type="success">success</wec-button>
-      <wec-button type="error">error</wec-button>
-      <wec-button type="text">text</wec-button>
+
+      <div class="button-item"><wec-button type="primary">primary</wec-button></div>
+      <div class="button-item"><wec-button type="info">info</wec-button></div>
+      <div class="button-item"><wec-button type="warn">warn</wec-button></div>
+      <div class="button-item"><wec-button type="success">success</wec-button></div>
+      <div class="button-item"><wec-button type="error">error</wec-button></div>
+      <div class="button-item"><wec-button type="text">text</wec-button></div>
   </section>
 </template>
+
+<style lang="scss" scoped>
+.demo-button {
+  .button-item {
+    margin-bottom: 10px;
+  }
+}
+</style>
--- packages/button/src/main.vue
+++ packages/button/src/main.vue
@@ -5,32 +5,34 @@
     </button>
 </template>
 <script>
-export default {
-  name: "wec-button",
-  props: {
-    // 尺寸
-    size: {
-      type: String,
-      default: "middle",
-      validator: function(value) {
-        // 值必须是这些字符串中的一个
-        return ["small", "middle", "large"].indexOf(value) !== -1;
+  export default {
+    name: "wec-button",
+    props: {
+      // 尺寸
+      size: {
+        type: String,
+        default: "middle",
+        validator: function(value) {
+          // 值必须是这些字符串中的一个
+          return ["small", "middle", "large","mini"].indexOf(value) !== -1;
+        }
+      },
+      // 类型
+      type: {
+        type: String,
+        default: "primary",
+        validator: function(value) {
+          return (
+            ["primary", "warn", "info", "success", "error", "text"].indexOf(
+              value
+            ) !== -1
+          );
+        }
       }
     },
-    // 类型
-    type: {
-      type: String,
-      default: "primary",
-      validator: function(value) {
-        return (
-          ["primary", "warn", "success", "error", "text"].indexOf(value) !== -1
-        );
-      }
-    }
-  },
-  data() {
-    return {};
-  },
-  computed: {}
-};
+    data() {
+      return {};
+    },
+    computed: {}
+  };
 </script>
