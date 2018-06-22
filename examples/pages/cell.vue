b55f404680a2e590ab8e178e498087c76bc4f427
edgardong <2535615874@qq.com>
2018-06-22 11:33:30 +0800

    add cell files

--- examples/pages/cell.vue
+++ examples/pages/cell.vue
@@ -2,10 +2,39 @@
 <template>
   <section class="demo-cell">
     <wec-header :show-back="true" title="Cell 单元格"></wec-header>
-    <wec-cell label="左边的label"></wec-cell>
-    <wec-cell label="左边的label" value="右边的值"></wec-cell>
+    <!-- 有icon的单元格 -->
+    <wec-cell label="左边的label" icon="icon-help" value="带 icon" link @click="clickHandler"></wec-cell>
+    <!-- 有注释的单元格 -->
+    <wec-cell label="左边的label" value="右边的值" comment="label注释"></wec-cell>
+    <!--  -->
+    <wec-cell label="左边的label" comment="label注释" icon="icon-more">
+      <div style="color:red;">这里是html元素</div>
+    </wec-cell>
+
+    <!--  -->
+    <wec-cell label="左边的label" comment="label注释" icon="icon-more">
+      <wec-button @click="buttonHandler">按钮哦</wec-button>
+    </wec-cell>
+    <!-- 带连接的单元格 -->
+    <wec-cell label="有链接哦" link value="带链接" to="http://blog.edgarhao.cn/" comment="连接到http://blog.edgarhao.cn"></wec-cell>
+    <wec-cell label="有链接哦" link value="带链接" :to="{path: '/component/tabbar'}" comment="连接到tabbar组件"></wec-cell>
   </section>
 </template>
+
+<script>
+export default {
+  methods: {
+    clickHandler() {
+      console.log('点击了单元格。。。')
+    },
+    buttonHandler() {
+      console.log('点击了按钮。。。')
+    }
+  }
+}
+</script>
+
+
 <style lang="scss" scoped>
 .demo-cell{
   padding-top: 46px;
--- packages/cell/src/main.vue
+++ packages/cell/src/main.vue
@@ -1,7 +1,23 @@
 <template>
-  <div class="wec-cell">
-    <span class="wec-cell--label">{{label}}</span>
-    <span class="wec-cell--value">{{value}}</span>
+  <div class="wec-cell" @click="clickHandler">
+    <div class="wec-cell__label">
+      <span>
+        <slot name="icon">
+          <i class="icon iconfont" :class="icon" v-if="icon"></i>
+        </slot>
+      </span>
+      <div class="wec-cell__label--label">
+        <span>{{label}}</span>
+        <span class="wec-cell__label--comment" v-if="comment">{{comment}}</span>
+      </div>
+    </div>
+    <div class="wec-cell__value" :class="{'has-link': link}">
+      <slot>
+        <span v-text="value"></span>
+      </slot>
+    </div>
+    <!-- 右侧箭头部分 -->
+    <i class="icon link-icon iconfont icon-more" v-if="link"></i>
   </div>
 </template>
 <script>
@@ -15,12 +31,47 @@ export default {
     value: {
       type: String,
       required: false
+    },
+    comment: {
+      type: String,
+      required: false
+    },
+    icon: {
+      type: String,
+      required: false
+    },
+    link: {
+      type: Boolean,
+      default: false,
+      required: false
+    },
+    to: {
+      type: [String, Object],
+      required: false
     }
   },
   data: function() {
     return {};
   },
-  methods: {},
+  methods: {
+    clickHandler($event) {
+      if (!this.link) {
+        $event.preventDefault();
+        return;
+      } else {
+        if (this.to) {
+          const resolved = this.$router.match(this.to);
+          if (!resolved.matched.length) {
+            location.href = this.to;
+            return;
+          }
+          this.$router.push(resolved.fullPath || resolved.path);
+        } else {
+          this.$emit("click");
+        }
+      }
+    }
+  },
   computed: {},
   mounted() {}
 };
