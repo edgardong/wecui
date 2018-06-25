cfd5305066e522d2ffd29bcfd291dfe0ed073b64
edgardong <2535615874@qq.com>
2018-06-25 23:01:26 +0800

    change checklist bug

--- examples/pages/checklist.vue
+++ examples/pages/checklist.vue
@@ -4,61 +4,61 @@
     <wec-header :show-back="true" title="Checklist 复选框"></wec-header>
 
     <wec-checklist v-model="value" :title="label" :options="options"></wec-checklist>
-    <wec-cell label="城市">{{value +' , '+(value?options[value-1].label:'')}}</wec-cell>
+    <wec-cell label="城市">{{value }}</wec-cell>
 
     <wec-checklist v-model="value1" :title="label1" :options="options1" align="right"></wec-checklist>
-    <wec-cell label="性别">{{value1 +' , '+ (value1?options1[value1-1].label:'')}}</wec-cell>
+    <wec-cell label="性别">{{value1}}</wec-cell>
   </section>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      value: "",
-      value1: "",
-      label: "选择区域",
-      label1: "选择性别",
-      options: [
-        {
-          label: "武汉市",
-          value: "1",
-          disabled: false
-        },
-        {
-          label: "襄阳市",
-          value: "2",
-          disabled: false
-        },
-        {
-          label: "其他",
-          value: "3",
-          disabled: true
-        }
-      ],
-      options1: [
-        {
-          label: "男",
-          value: "1",
-          disabled: false
-        },
-        {
-          label: "女",
-          value: "2",
-          disabled: false
-        },
-        {
-          label: "其他",
-          value: "3",
-          disabled: true
-        }
-      ]
-    };
-  }
-};
+  export default {
+    data() {
+      return {
+        value: "",
+        value1: "",
+        label: "选择区域",
+        label1: "选择性别",
+        options: [
+          {
+            label: "武汉市",
+            value: "1",
+            disabled: false
+          },
+          {
+            label: "襄阳市",
+            value: "2",
+            disabled: false
+          },
+          {
+            label: "其他",
+            value: "3",
+            disabled: false
+          }
+        ],
+        options1: [
+          {
+            label: "男",
+            value: "1",
+            disabled: false
+          },
+          {
+            label: "女",
+            value: "2",
+            disabled: false
+          },
+          {
+            label: "其他",
+            value: "3",
+            disabled: true
+          }
+        ]
+      };
+    }
+  };
 </script>
 <style lang="scss" scoped>
-.demo-checklist {
-  padding-top: 46px;
-}
+  .demo-checklist {
+    padding-top: 46px;
+  }
 </style>
--- packages/checklist/src/main.vue
+++ packages/checklist/src/main.vue
@@ -3,60 +3,58 @@
     <span class="wec-checklist__title">{{title}}</span>
     <wec-cell v-for="(op,index) in options" :key="index" :label="op.label" @click="clickHandler(op)">
       <span slot="left" class="wec-checklist__core"
-       :class="{'checked': currentValue==op.value,'disabled':op.disabled,'right':align=='right'}">
+       :class="{'checked': currentValue.includes(op.value),'disabled':op.disabled,'right':align=='right'}">
         <input type="checkbox" :value="op.value" class="wec-checklist__core--input" v-model="currentValue">
       </span>
     </wec-cell>
   </div>
 </template>
 <script>
-export default {
-  name: "wec-checklist",
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-    options: {
-      type: [Array, Object],
-      required: true
-    },
-    value: {
-      type: [String, Object]
-    },
-    align: {
-      type: String,
-      default: "left",
-      required: false,
-      validator: value => {
-        return ["left", "right"].includes(value);
+  export default {
+    name: "wec-checklist",
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      options: {
+        type: [Array, Object],
+        required: true
+      },
+      value: {
+        type: [String, Object]
+      },
+      align: {
+        type: String,
+        default: "left",
+        required: false,
+        validator: value => {
+          return ["left", "right"].includes(value);
+        }
       }
-    }
-  },
-  data() {
-    return {
-      currentValue: this.value
-    };
-  },
-  watch: {
-    value(val) {
-      this.currentValue = val;
     },
-    currentValue(val) {
-      // console.log(val);
-      this.$emit("input", val);
-    }
-  },
-  methods: {
-    clickHandler(option) {
-      // console.log(option);
-      if (option.disabled) {
-        return;
+    data() {
+      return {
+        currentValue: this.value
+      };
+    },
+    watch: {
+      value(val) {
+        this.currentValue = val;
+      },
+      currentValue(val) {
+        this.$emit("input", val);
       }
-      this.currentValue = option.value;
-    }
-  },
-  computed: {},
-  mounted() {}
-};
+    },
+    methods: {
+      clickHandler(option) {
+        if (option.disabled) {
+          return;
+        }
+        this.currentValue += option.value;
+      }
+    },
+    computed: {},
+    mounted() {}
+  };
 </script>
