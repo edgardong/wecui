b3552ad5bd0a3595fad44d80592beb6b7ad32c59
edgardong <2535615874@qq.com>
2018-07-16 14:15:03 +0800

    fixed range files

--- examples/pages/range.vue
+++ examples/pages/range.vue
@@ -2,7 +2,15 @@
 <template>
   <section class="demo-range">
     <wec-header :show-back="true" title="Range 滑动"></wec-header>
-    <wec-range :step="11" :min="min" :max="max">
+
+    <h2>基本用法</h2>
+    <wec-range :step="11" :min="min" :max="max" v-model="value" :label="'滑块 ' + value">
+      <span slot="left">{{min}}</span>
+      <span slot="right">{{max}}</span>
+    </wec-range>
+
+    <h2>禁用状态</h2>
+    <wec-range :step="11" :min="min" :max="max" :disabled="true" v-model="value1" :label="'滑块 ' + value1">
       <span slot="left">{{min}}</span>
       <span slot="right">{{max}}</span>
     </wec-range>
@@ -13,8 +21,10 @@
     props: {},
     data() {
       return {
+        value:40,
         min: 10,
-        max: 100
+        max: 100,
+        value1:30
       };
     },
     methods: {},
@@ -25,5 +35,10 @@
 <style lang="scss" scoped>
   .demo-range {
     padding-top: 46px;
+  }
+
+  h2 {
+    text-align: center;
+    margin: 15px 0;
   }
 </style>
--- packages/range/src/main.vue
+++ packages/range/src/main.vue
@@ -2,10 +2,14 @@
   <div class="wec-range">
     <wec-cell :label="label">
       <div class="wec-range__content">
+        <div class="wec-range__content--masker" v-show="disabled"></div>
         <!-- 左侧显示 -->
         <slot name="left" v-if="showLable"></slot>
         <!-- 中间滑块部分 -->
         <div class="wec-range__content--wrapper" ref="content">
+          
+          <!-- 进度条 -->
+          <span class="wrapper--progress" :style="{width: (slider?slider.style.left: 0)}"></span>
           <!-- 中间的滑块 -->
           <span class="wrapper--slider" ref="slider" :class="[sliderClass]"></span>
           <span class="wrapper--scale" :class="{'first--scale':index==0}" v-for="(n,index) in scaleCount" :key="index"></span>
@@ -17,130 +21,177 @@
   </div>
 </template>
 <script>
-  export default {
-    name: "wec-range",
-    props: {
-      label: {
-        type: String,
-        default: "滑块"
-      },
-      step: {
-        type: Number,
-        default: 10
-      },
-      sliderType: {
-        type: [String, Number],
-        default: "circle"
-      },
-      min: {
-        type: Number,
-        default: 0
-      },
-      max: {
-        type: Number,
-        default: 100
-      },
-      showLable: {
-        type: Boolean,
-        default: true
-      },
-      value: {}
+export default {
+  name: "wec-range",
+  props: {
+    label: {
+      type: String,
+      default: ""
     },
-    data() {
-      return {
-        slider: null,
-        dragState: {
-          startX: 0,
-          endX: 0,
-          sliderStartX: 0
-        }
-      };
+    step: {
+      type: Number,
+      default: 10
     },
-    methods: {},
-    computed: {
-      sliderClass() {
-        if (this.sliderType == "circle") {
-        }
-        return "wrapper-slider--cicle";
-      },
-      // 计算刻度尺的数量
-      scaleCount() {
-        return Math.ceil((this.max - this.min) / this.step);
+    sliderType: {
+      type: [String, Number],
+      default: "circle"
+    },
+    min: {
+      type: Number,
+      default: 0
+    },
+    max: {
+      type: Number,
+      default: 100
+    },
+    showLable: {
+      type: Boolean,
+      default: true
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    value: {}
+  },
+  data() {
+    return {
+      slider: null,
+      dragState: {
+        startX: 0,
+        endX: 0,
+        sliderStartX: 0
       }
-    },
-    mounted() {
+    };
+  },
+  methods: {
+    setSlider(value) {
       let _this = this;
-      this.$nextTick(() => {
-        this.slider = this.$refs.slider;
 
-        let parent = this.$refs.content.getBoundingClientRect();
-        let sliderObj = this.slider.getBoundingClientRect();
-        console.log(this.slider);
-        // 开始移动
-        this.slider.addEventListener("touchstart", function(event) {
-          let touch = event.touches[0];
-          _this.dragState.startX = touch.pageX;
-          _this.dragState.sliderStartX = sliderObj.left;
-        });
+      this.slider = this.$refs.slider;
 
-        // 移动过程中
-        this.slider.addEventListener("touchmove", function(event) {
-          let touch = event.touches[0];
-          _this.dragState.endX = touch.pageX;
+      let parent = _this.$refs.content.getBoundingClientRect();
+      let sliderObj = _this.slider.getBoundingClientRect();
 
-          let sliderHalfWidth = sliderObj.width / 2;
+      let sliderHalfWidth = sliderObj.width / 2;
 
-          // 获取每一步的步长
-          let stepWidth = Math.ceil(sliderObj.width / _this.step);
-          // 移动的距离
-          let moveX = _this.dragState.endX - _this.dragState.startX;
-          let leftOffset = 0;
-          // console.group("slider...");
-          // console.log(parent);
-          // console.log(sliderObj);
-          // console.groupEnd();
-          // // 获取滑动的距离，最小为0，最大为父级元素的宽度
-          // console.group("juli....");
-          // console.log(_this.slider.style.left);
-          console.log(_this.dragState.sliderStartX);
-          // console.log("移动距离moveX.... " + moveX);
-          // console.log("父元素left.... " + parent.left);
-          // console.log("slider  left .... " + sliderObj.left);
-          // // console.log("..........+++  " + sliderStartX);
-
-          console.log(moveX);
-          if (moveX > 0) {
-            leftOffset = Math.min(
-              moveX,
-              parent.width - sliderHalfWidth - (sliderObj.left - parent.left)
-            );
-          } else {
-            leftOffset = Math.max(
-              -sliderHalfWidth,
-              Math.max(-sliderHalfWidth, _this.dragState.sliderStartX + moveX)
-            );
-          }
-
-          // consolo.log();
-
-          _this.slider.style.left =
-            _this.dragState.sliderStartX - parent.left + leftOffset + "px";
-        });
-
-        this.slider.addEventListener("touchend", function(event) {
-          // 获取父元素，根据父元素来确定可以滑动的距离
-          // let parent = _this.$refs.content.getBoundingClientRect();
-          // let moveX = _this.dragState.endX - _this.dragState.startX;
-          // let leftOffset = 0;
-          // leftOffset = Math.min(Math.max(-7, moveX), parent.left);
-          // _this.slider.style.left = leftOffset + "px";
-        });
-      });
+      let progress = Math.floor(
+        (value - _this.min) / (_this.max - _this.min) * 100
+      );
+      _this.slider.style.left =
+        progress / 100 * parent.width - sliderHalfWidth + "px";
+    }
+  },
+  computed: {
+    // 滑块样式，暂时只提供圆形一种
+    sliderClass() {
+      if (this.sliderType == "circle") {
+      }
+      return "wrapper-slider--cicle";
     },
-    watch: {
-      value(value) {
+    // 计算刻度尺的数量
+    scaleCount() {
+      return Math.ceil((this.max - this.min) / this.step);
+    },
+    currentValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
         this.$emit("input", value);
       }
     }
-  };
+  },
+  mounted() {
+    let _this = this;
+    // 设置默认刻度值
+    this.setSlider(this.currentValue);
+
+    this.$nextTick(() => {
+      this.slider = this.$refs.slider;
+
+      let getSliderPosition = () => {
+        let parent = this.$refs.content.getBoundingClientRect();
+        let sliderObj = this.slider.getBoundingClientRect();
+
+        return {
+          left: sliderObj.left - parent.left
+        };
+      };
+
+      // 开始移动
+      this.slider.addEventListener("touchstart", function(event) {
+        if (_this.disabled) {
+          return;
+        }
+
+        let parent = _this.$refs.content.getBoundingClientRect();
+        let sliderObj = _this.slider.getBoundingClientRect();
+
+        let touch = event.touches[0];
+        // let position = getSliderPosition();
+        // 定义鼠标开始移动时的起点
+        _this.dragState.startX = touch.pageX;
+        // 获取滑块开始移动式的left值
+        _this.dragState.sliderStartX = sliderObj.left - parent.left;
+      });
+
+      // 移动过程中
+      this.slider.addEventListener("touchmove", function(event) {
+        if (_this.disabled) {
+          return;
+        }
+
+        let parent = _this.$refs.content.getBoundingClientRect();
+        let sliderObj = _this.slider.getBoundingClientRect();
+
+        let touch = event.touches[0];
+        _this.dragState.endX = touch.pageX;
+
+        let sliderHalfWidth = sliderObj.width / 2;
+
+        // 获取每一步的步长
+        let stepWidth = Math.ceil(sliderObj.width / _this.step);
+        // 移动的距离
+        let moveX = _this.dragState.endX - _this.dragState.startX;
+        let leftOffset = moveX + _this.dragState.sliderStartX;
+
+        let newPosition = Math.max(0, Math.min(leftOffset, parent.width - 7));
+
+        let newProgress = newPosition / (parent.width - sliderHalfWidth * 2);
+
+        if (newProgress < 0) {
+          newProgress = 0;
+        } else if (newProgress > 1) {
+          newProgress = 1;
+        }
+
+        let value = Math.round(
+          _this.min + newProgress * (_this.max - _this.min)
+        );
+
+        // 回传数量
+        _this.$emit("input", value);
+
+        let progress = Math.floor(
+          (value - _this.min) / (_this.max - _this.min) * 100
+        );
+        _this.slider.style.left =
+          progress / 100 * parent.width - sliderHalfWidth + "px";
+      });
+
+      this.slider.addEventListener("touchend", function(event) {
+        if (_this.disabled) {
+          return;
+        }
+        // 获取父元素，根据父元素来确定可以滑动的距离
+        // let parent = _this.$refs.content.getBoundingClientRect();
+        // let moveX = _this.dragState.endX - _this.dragState.startX;
+        // let leftOffset = 0;
+        // leftOffset = Math.min(Math.max(-7, moveX), parent.left);
+        // _this.slider.style.left = leftOffset + "px";
+      });
+    });
+  }
+};
 </script>
