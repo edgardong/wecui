c055ebb29451b40a040cabb1e7f979442c683243
edgardong <2535615874@qq.com>
2018-07-17 22:45:04 +0800

    add fiels

--- examples/pages/picker.vue
+++ examples/pages/picker.vue
@@ -8,6 +8,13 @@
     <wec-picker ref="picker" v-model="values" @pickok="changeHandler" title="选择方式" :slots="slots"></wec-picker>
 
     {{values.join('')}}
+
+
+    <wec-button @click="openPickerHandler1" size="block" class="flex1" type="success"> 显示选择框1</wec-button>
+
+    <wec-picker ref="picker1" v-model="values1" @pickok="changeHandler1" title="选择方式" :slots="slots1"></wec-picker>
+
+    {{values1.join('')}}
   </section>
 </template>
 <script>
@@ -15,9 +22,22 @@
     props: {},
     data() {
       return {
-        showPicker: true,
         values: ["襄阳", "广州"],
+        values1: ["武汉", "杭州"],
         slots: [
+          {
+            options: ["武汉", "襄阳", "苏州", "上海", "南漳", "淮南", "凤台"],
+            defaultIndex: 4
+          },
+          {
+            divider: true,
+            content: "-"
+          },
+          {
+            options: ["杭州", "南京", "常州", "深圳", "广州"]
+          }
+        ],
+        slots1: [
           {
             options: ["武汉", "襄阳", "苏州", "上海", "南漳", "淮南", "凤台"],
             defaultIndex: 4
@@ -34,10 +54,15 @@
     },
     methods: {
       openPickerHandler() {
-        this.showPicker = true;
         this.$refs.picker.show();
       },
       changeHandler(picker, values) {
+        this.values = values;
+      },
+      openPickerHandler1() {
+        this.$refs.picker1.show();
+      },
+      changeHandler1(picker, values) {
         this.values = values;
       }
     },
