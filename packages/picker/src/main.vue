a565eca6d81b4786eae38caa3d14ebf438593275
edgardong <2535615874@qq.com>
2018-07-17 22:41:20 +0800

    fixed picker bugs

--- packages/picker/src/main.vue
+++ packages/picker/src/main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wec-picker" v-if="visiable && currentVisiable" @click="maskerHandler">
+  <div class="wec-picker" v-if="currentVisiable" @click="maskerHandler">
     <div class="wec-picker__wrapper" @click.stop>
       <!-- 顶部操作区域 -->
       <div class="wec-picker__wrapper--toolbar">
@@ -12,7 +12,7 @@
         <div class="picker-wrapper">
           <div class="scroll-wrapper">
             <div class="picker-slot">
-              <picker-item :slotObj="slot" v-model="values[slot.slotIndex]" :class="{'divider-item':slot.divider,'content-item': !slot.divider}" v-for="(slot,index) in slots" :key="index"></picker-item>
+              <picker-item :slotObj="slot" v-model="pickerValues[slot.slotIndex]" :class="{'divider-item':slot.divider,'content-item': !slot.divider}" v-for="(slot,index) in slots" :key="index"></picker-item>
               <!-- 中部高亮区域 -->
               <div class="picker-slot--highlight" ref="highlight"></div>
             </div>
@@ -23,79 +23,82 @@
   </div>
 </template>
 <script>
-import PickerItem from "./picker-item";
-export default {
-  name: "wec-picker",
-  props: {
-    visiable: {
-      type: Boolean,
-      default: true
-    },
-    title: {
-      type: String
-    },
-    slots: {
-      type: Array,
-      required: true
-    },
-    onModalClose: {
-      type: Boolean,
-      default: false
-    },
-    itemCount: {
-      type: Number,
-      default: 5
-    },
-    itemHeight: {
-      type: Number,
-      default: 36
-    },
-    value: {
-      type: Array
-    }
-  },
-  data() {
-    return {
-      values: this.value,
-      currentVisiable: this.visiable,
-      pickerValues: []
-    };
-  },
-  methods: {
-    cancelHandler() {
-      this.currentVisiable = false;
-    },
-    sureHandler() {
-      console.log(this.values);
-    },
-    show() {
-      this.currentVisiable = true;
-    },
-    maskerHandler() {
-      if (!this.onModalClose) {
-        return;
+  import PickerItem from "./picker-item";
+  export default {
+    name: "wec-picker",
+    props: {
+      title: {
+        type: String
+      },
+      slots: {
+        type: Array,
+        required: true
+      },
+      onModalClose: {
+        type: Boolean,
+        default: false
+      },
+      itemCount: {
+        type: Number,
+        default: 5
+      },
+      itemHeight: {
+        type: Number,
+        default: 36
+      },
+      value: {
+        type: Array
       }
-      this.currentVisiable = false;
-    }
-  },
-  computed: {},
-  mounted() {
-    let highlight = this.$refs.highlight;
-    let centerPosition = highlight.getBoundingClientRect();
-    console.log(centerPosition);
-
-    // slots动态添加slotIndex元素,去掉divider的Index
-    let _this = this;
-    let count = 0;
-    this.slots.forEach((slot, index) => {
-      if (!slot.divider) {
-        _this.$set(_this.slots[index], "slotIndex", count);
-        count++;
+    },
+    data() {
+      return {
+        pickerValues: [],
+        currentVisiable: false
+      };
+    },
+    created() {
+      this.value.forEach((val, index) => {
+        this.pickerValues[index] = val;
+      });
+    },
+    methods: {
+      getValues() {
+        // return this.pickerValues.concat();
+      },
+      cancelHandler() {
+        this.currentVisiable = false;
+      },
+      sureHandler() {
+        // this.values1 = this.pickerValues;
+        this.$emit("input", this.pickerValues.concat());
+        this.currentVisiable = false;
+        // this.$destroy();
+      },
+      show() {
+        this.currentVisiable = true;
+      },
+      maskerHandler() {
+        if (!this.onModalClose) {
+          return;
+        }
+        this.currentVisiable = false;
       }
-    });
-  },
-  components: {
-    PickerItem
-  }
-};
+    },
+    computed: {},
+    mounted() {
+      let highlight = this.$refs.highlight;
+      // slots动态添加slotIndex元素,去掉divider的Index
+      let _this = this;
+      let count = 0;
+      this.slots.forEach((slot, index) => {
+        if (!slot.divider) {
+          _this.$set(_this.slots[index], "slotIndex", count);
+          count++;
+        }
+      });
+    },
+    components: {
+      PickerItem
+    }
+  };
 </script>
--- packages/picker/src/picker-item.vue
+++ packages/picker/src/picker-item.vue
@@ -9,127 +9,123 @@
   </div>
 </template>
 <script>
-export default {
-  props: {
-    slotObj: {
-      type: Object,
-      required: true
-    },
-    value: {}
-  },
-  data() {
-    return {
-      currentIndex: this.slotObj.defaultIndex || 0
-    };
-  },
-  methods: {
-    // 获取指定元素translate距离
-    getTranslatePosition(el) {
-      let style = el.style.transform;
-      let position = { top: 0, left: 0 };
-      let matches = /translate\(\s*(\-?\d+)px,\s*(\-?\d+)px\)\s*/g.exec(style);
-      if (matches) {
-        position.left = +matches[1];
-        position.top = +matches[2];
-        return position;
-      }
-    },
-    getCurrentValue(index) {
-      return this.slotObj && this.slotObj.options
-        ? this.slotObj.options[index]
-        : "";
-    }
-  },
-  computed: {
-    currentValue: {
-      get() {
-        return this.getCurrentValue(this.currentIndex);
+  export default {
+    props: {
+      slotObj: {
+        type: Object,
+        required: true
       },
-      set(value) {
-        this.$emit("input", value);
+      value: {}
+    },
+    data() {
+      return {
+        currentIndex: this.slotObj.defaultIndex || 0,
+      };
+    },
+    methods: {
+      // 获取指定元素translate距离
+      getTranslatePosition(el) {
+        let style = el.style.transform;
+        let position = { top: 0, left: 0 };
+        let matches = /translate\(\s*(\-?\d+)px,\s*(\-?\d+)px\)\s*/g.exec(style);
+        if (matches) {
+          position.left = +matches[1];
+          position.top = +matches[2];
+          return position;
+        }
+      },
+      getCurrentValue(index) {
+        return this.slotObj && this.slotObj.options
+          ? this.slotObj.options[index]
+          : "";
       }
-    }
-  },
-  watch: {
-    currentValue(value) {
-      this.$emit("input", value);
-    }
-  },
-  created() {
-    let value = "";
-    value = this.value;
-    if (!value) {
-      value = this.getCurrentValue(this.currentIndex);
-    }
-    if (value) {
-      this.$emit("input", value);
-    }
-  },
-  mounted() {
-    let _this = this;
-    let drag = {};
-    this.$nextTick(() => {
-      let scroll = this.$refs.scrollwrapper;
-      if (this.slotObj.divider) {
-        return;
+    },
+    created() {
+      let value = this.value;
+      if (!value) {
+        value = this.getCurrentValue(this.currentIndex);
       }
-      // 默认平移到第一个选择项处
-      scroll.style.transform = "translate(0,72px)";
-      // 获取默认选择的选项
-      let pickerItems = scroll.querySelector(".picker-selected");
-      // 把选项平移到中间的位置
-      if (pickerItems) {
-        scroll.style.transform = `translate(0,${72 - pickerItems.offsetTop}px)`;
-        scroll.style.transition = `transform 0.5s ease-in-out`;
+      if (value) {
+        this.currentValue = value;
       }
-
-      // 滑动开始移动事件
-      scroll.addEventListener("touchstart", function(event) {
-        let touch = event.touches[0];
-        let scroll = _this.$refs.scrollwrapper;
-        drag.startY = touch.pageY;
-        drag.scrollTop = _this.getTranslatePosition(scroll).top;
-      });
-
-      // 滑动移动过程中的事件
-      scroll.addEventListener("touchmove", function(event) {
-        let touch = event.touches[0];
-        drag.endY = touch.pageY;
-        // 计算移动的距离
-        let moveY = drag.endY - drag.startY;
-        // 计算移动的个数
-        let moveIndex = Math.round(moveY / 36);
-
-        // 计算顶部的滚动距离
-        let offsetTop = drag.scrollTop + moveIndex * 36;
-
-        // 设置可以滚动的最大和最小距离
-        if (offsetTop <= 72 - (_this.slotObj.options.length - 1) * 36) {
-          offsetTop = 72 - (_this.slotObj.options.length - 1) * 36;
-        } else if (offsetTop >= 72) {
-          offsetTop = 72;
+    },
+    watch: {
+    },
+    computed: {
+      currentValue: {
+        get() {
+          return this.value;
+        },
+        set(value) {
+          this.$emit("input", value);
+        }
+      }
+    },
+    mounted() {
+      let _this = this;
+      let drag = {};
+      this.$nextTick(() => {
+        let scroll = this.$refs.scrollwrapper;
+        if (this.slotObj.divider) {
+          return;
+        }
+        // 默认平移到第一个选择项处
+        scroll.style.transform = "translate(0,72px)";
+        // 获取默认选择的选项
+        let pickerItems = scroll.querySelector(".picker-selected");
+        // 把选项平移到中间的位置
+        if (pickerItems) {
+          scroll.style.transform = `translate(0,${72 - pickerItems.offsetTop}px)`;
+          scroll.style.transition = `transform 0.5s ease-in-out`;
         }
 
-        scroll.style.transform = `translate(0,${offsetTop}px)`;
-
-        scroll.addEventListener("transitionend", function(event) {
-          let newIndex = (72 - offsetTop) / 36;
-
-          // 设置当前索引
-          _this.currentIndex = Math.max(
-            0,
-            Math.min(newIndex, _this.slotObj.options.length - 1)
-          );
-
-          // _this.currentValue = _this.slotObj.options[_this.currentIndex];
+        // 滑动开始移动事件
+        scroll.addEventListener("touchstart", function(event) {
+          let touch = event.touches[0];
+          let scroll = _this.$refs.scrollwrapper;
+          drag.startY = touch.pageY;
+          drag.scrollTop = _this.getTranslatePosition(scroll).top;
         });
 
-        scroll.removeEventListener("transitionend", function(event) {});
-      });
+        // 滑动移动过程中的事件
+        scroll.addEventListener("touchmove", function(event) {
+          let touch = event.touches[0];
+          drag.endY = touch.pageY;
+          // 计算移动的距离
+          let moveY = drag.endY - drag.startY;
+          // 计算移动的个数
+          let moveIndex = Math.round(moveY / 36);
 
-      // 滑动移动结束后事件
-      scroll.addEventListener("touchend", function(event) {});
-    });
-  }
-};
+          // 计算顶部的滚动距离
+          let offsetTop = drag.scrollTop + moveIndex * 36;
+
+          // 设置可以滚动的最大和最小距离
+          if (offsetTop <= 72 - (_this.slotObj.options.length - 1) * 36) {
+            offsetTop = 72 - (_this.slotObj.options.length - 1) * 36;
+          } else if (offsetTop >= 72) {
+            offsetTop = 72;
+          }
+
+          scroll.style.transform = `translate(0,${offsetTop}px)`;
+
+          scroll.addEventListener("transitionend", function(event) {
+            let newIndex = (72 - offsetTop) / 36;
+
+            // 设置当前索引
+            _this.currentIndex = Math.max(
+              0,
+              Math.min(newIndex, _this.slotObj.options.length - 1)
+            );
+
+            _this.currentValue = _this.getCurrentValue(_this.currentIndex);
+          });
+
+          scroll.removeEventListener("transitionend", function(event) {});
+        });
+
+        // 滑动移动结束后事件
+        scroll.addEventListener("touchend", function(event) {});
+      });
+    }
+  };
 </script>
