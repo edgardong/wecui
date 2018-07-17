1c16e74e6025bc500cb384a6e61d4e8a2c28d238
edgardong <2535615874@qq.com>
2018-07-17 18:44:37 +0800

    add pciker files

--- examples/pages/picker.vue
+++ examples/pages/picker.vue
@@ -2,43 +2,65 @@
 <template>
   <section class="demo-picker">
     <wec-header :show-back="true" title="Picker 选择器"></wec-header>
-    <wec-button @click="openPickerHandler" size="block">显示选择框</wec-button>
-    <wec-picker ref="picker" title="选择方式" :slots="slots" :visiable="showPicker"></wec-picker>
+
+    <wec-button @click="openPickerHandler" size="block" class="flex1" type="success"> 显示选择框</wec-button>
+
+    <wec-picker ref="picker" v-model="values" title="选择方式" :slots="slots" :visiable="showPicker"></wec-picker>
   </section>
 </template>
 <script>
-  export default {
-    props: {},
-    data() {
-      return {
-        showPicker: true,
-        slots: [
-          {
-            options: ["武汉", "襄阳", "苏州", "上海", "南漳", "淮南", "凤台"]
-          },
-          {
-            divider: true,
-            content: "-"
-          },
-          {
-            options: ["杭州", "南京", "常州", "深圳", "广州"]
-          }
-        ]
-      };
-    },
-    methods: {
-      openPickerHandler() {
-        this.showPicker = true;
-        console.log(this.$refs.picker)
-        this.$refs.picker.show();
-      }
-    },
-    computed: {},
-    mounted() {}
-  };
+export default {
+  props: {},
+  data() {
+    return {
+      showPicker: true,
+      values: ["襄阳", "广州"],
+      slots: [
+        {
+          options: ["武汉", "襄阳", "苏州", "上海", "南漳", "淮南", "凤台"],
+          defaultIndex: 4
+        },
+        {
+          divider: true,
+          content: "-"
+        },
+        {
+          options: ["杭州", "南京", "常州", "深圳", "广州"]
+        }
+      ]
+    };
+  },
+  methods: {
+    openPickerHandler() {
+      this.showPicker = true;
+      console.log(this.$refs.picker);
+      this.$refs.picker.show();
+    }
+  },
+  computed: {},
+  mounted() {}
+};
 </script>
 <style lang="scss" scoped>
-  .demo-picker {
-    padding-top: 46px;
+.demo-picker {
+  padding-top: 46px;
+  margin: 0 20px;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  /*水平居中*/
+  align-items: center;
+  flex-direction: column;
+  .flex1 {
+    margin: 10px 0;
+    color: #656b79;
+    border-color: #f6f8fa;
+    background-color: #f6f8fa;
   }
+}
 </style>
--- packages/picker/src/main.vue
+++ packages/picker/src/main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wec-picker" v-show="visiable && currentVisiable" @click="maskerHandler">
+  <div class="wec-picker" v-if="visiable && currentVisiable" @click="maskerHandler">
     <div class="wec-picker__wrapper" @click.stop>
       <!-- 顶部操作区域 -->
       <div class="wec-picker__wrapper--toolbar">
@@ -11,10 +11,11 @@
       <div class="wec-picker__wrapper--content" :style="{height:itemCount*itemHeight+'px'}">
         <div class="picker-wrapper">
           <div class="scroll-wrapper">
-            <div class="picker-slot" :class="{'divider-item':slot.divider,'content-item': !slot.divider}" v-for="(slot,index) in slots" :key="index">
-              <picker-item :slotObj="slot"></picker-item>
+            <div class="picker-slot">
+              <picker-item :slotObj="slot" v-model="values[slot.slotIndex]" :class="{'divider-item':slot.divider,'content-item': !slot.divider}" v-for="(slot,index) in slots" :key="index"></picker-item>
+              <!-- 中部高亮区域 -->
+              <div class="picker-slot--highlight" ref="highlight"></div>
             </div>
-            <div class="picker-slot--highlight" ref="highlight"></div>
           </div>
         </div>
       </div>
@@ -22,63 +23,79 @@
   </div>
 </template>
 <script>
-  import PickerItem from "./picker-item";
-  export default {
-    name: "wec-picker",
-    props: {
-      visiable: {
-        type: Boolean,
-        default: true
-      },
-      title: {
-        type: String
-      },
-      slots: {
-        type: Array,
-        required: true
-      },
-      onModalClose: {
-        type: Boolean,
-        default: true
-      },
-      itemCount: {
-        type: Number,
-        default: 5
-      },
-      itemHeight: {
-        type: Number,
-        default: 36
-      }
+import PickerItem from "./picker-item";
+export default {
+  name: "wec-picker",
+  props: {
+    visiable: {
+      type: Boolean,
+      default: true
     },
-    data() {
-      return {
-        values: ["武汉", "", "南京"],
-        currentVisiable: this.visiable
-      };
+    title: {
+      type: String
     },
-    methods: {
-      cancelHandler() {
-        this.currentVisiable = false;
-      },
-      sureHandler() {},
-      show() {
-        this.currentVisiable = true;
-      },
-      maskerHandler() {
-        if (!this.onModalClose) {
-          return;
-        }
-        this.currentVisiable = false;
-      }
+    slots: {
+      type: Array,
+      required: true
     },
-    computed: {},
-    mounted() {
-      let highlight = this.$refs.highlight;
-      let centerPosition = highlight.getBoundingClientRect();
-      console.log(centerPosition);
+    onModalClose: {
+      type: Boolean,
+      default: false
     },
-    components: {
-      PickerItem
+    itemCount: {
+      type: Number,
+      default: 5
+    },
+    itemHeight: {
+      type: Number,
+      default: 36
+    },
+    value: {
+      type: Array
     }
-  };
+  },
+  data() {
+    return {
+      values: this.value,
+      currentVisiable: this.visiable,
+      pickerValues: []
+    };
+  },
+  methods: {
+    cancelHandler() {
+      this.currentVisiable = false;
+    },
+    sureHandler() {
+      console.log(this.values);
+    },
+    show() {
+      this.currentVisiable = true;
+    },
+    maskerHandler() {
+      if (!this.onModalClose) {
+        return;
+      }
+      this.currentVisiable = false;
+    }
+  },
+  computed: {},
+  mounted() {
+    let highlight = this.$refs.highlight;
+    let centerPosition = highlight.getBoundingClientRect();
+    console.log(centerPosition);
+
+    // slots动态添加slotIndex元素,去掉divider的Index
+    let _this = this;
+    let count = 0;
+    this.slots.forEach((slot, index) => {
+      if (!slot.divider) {
+        _this.$set(_this.slots[index], "slotIndex", count);
+        count++;
+      }
+    });
+  },
+  components: {
+    PickerItem
+  }
+};
 </script>
